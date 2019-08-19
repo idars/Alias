32b2584f08db81a01a659a8b863615c5c802ddd9
Idar S <idars@pm.me>
2019-08-19 15:52:44 +0200

    Add inactive-functionality to EmojiStatus

--- src/components/emojis/EmojiStatus.js
+++ src/components/emojis/EmojiStatus.js
@@ -3,25 +3,30 @@ import Twemoji from 'react-twemoji';
 import styled, { withTheme } from 'styled-components'
 
 const StyledTwemoji = styled(Twemoji)`
-    opacity: ${state => (state.active ? '1.0' : '0.5')};
+    opacity: ${props => props.inactive ? '0.5' : '1.0'};
 
     img {
+		filter: ${props => props.inactive ? 'grayscale(100%)' : 'none'};
 		width: 24px;
 	}
 `;
 
 class EmojiStatus extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: true
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			inactive: !this.props.children
+		};
+	}
+
+	init() {
+		return this.state.inactive ? "😀" : this.props.children;
+	}
 
 	render() {
 		return (
 			<div className={this.props.className}>
-				<StyledTwemoji>{this.props.children || "😀"}</StyledTwemoji>
+				<StyledTwemoji inactive={this.state.inactive}>{this.init()}</StyledTwemoji>
 			</div>
 		);
 	}
